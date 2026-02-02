9636c9c7b67f9d51e620420b6d7582f834f5631c
Illia Dubina <armahanz.music@gmail.com>
2026-02-02 16:30:02 +0200

    Solution

---
+++ blog/urls.py
@@ -0,0 +1,27 @@
+from django.urls.conf import path
+from blog.views import (
+    index,
+    PostListView,
+    PostDetailView,
+    my_posts_view,
+    CommentaryCreateView
+)
+
+urlpatterns = [
+    path("", index, name="index"),
+    path("posts/", PostListView.as_view(), name="post-list"),
+    path("my-posts/", my_posts_view, name="my-posts-view"),
+    # path("post/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
+    path(
+        "post/<int:pk>/",
+        PostDetailView.as_view(),
+        name="post_detail.html-detail"
+    ),
+    path(
+        "post/<int:pk>/comment/",
+        CommentaryCreateView.as_view(),
+        name="comment-create"
+    ),
+]
+
+app_name = "blog"
